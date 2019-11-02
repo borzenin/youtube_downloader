1de3969e740f1e4a5d5f846e0f1e6e59d3ca69c4
Dmitry Borzenin <fked9181@yahoo.com>
2019-11-02 17:10:38 +0300

    Add token session migration

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -129,7 +129,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTTokenUserAuthentication',
-    )
+    ),
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
 
 SIMPLE_JWT = {
