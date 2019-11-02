6152ef8999770c29765918d0632c622e138df2ff
Dmitry Borzenin <fked9181@yahoo.com>
2019-11-02 17:45:51 +0300

    Change travis ci config and test settings

--- backend/config/settings/test.py
+++ backend/config/settings/test.py
@@ -9,3 +9,9 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/dev/ref/settings/#password-hashers
 PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
 
+DATABASES = {
+    'default': env.db(
+        'DATABASE_URL',
+        default='postgres://django_user:0000@127.0.0.1:5432'
+    ),
+}
