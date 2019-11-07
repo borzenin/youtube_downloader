94aee05a2523b884f2d0333d61303db0bd1093b7
Dmitry Borzenin <fked9181@yahoo.com>
2019-11-07 20:34:26 +0300

    Add urls for react js

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,9 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
+from django.shortcuts import render
+
+
+def base_view(request):
+    return render(request, 'base.html')
+
 
 urlpatterns = [
     path('api/v1/accounts/', include('accounts.urls')),
-    path('admin/', admin.site.urls),
+
+    path('', base_view),
+    path('login/', base_view),
+    path('register/', base_view),
+    path('about/', base_view),
+
+    path('admin/', admin.site.urls)
 ]
